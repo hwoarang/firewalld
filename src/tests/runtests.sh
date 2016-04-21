76fd8ce0c3a730044bc57989a63932e14848289f
Markos Chandras <mchandras@suse.de>
2016-04-21 13:27:18 +0100

    tests: runtest.sh: Add simple script to run the tests
    Add simple script to run the tests. This can be used to automate
    the testing process (eg Travis-CI)

---
+++ src/tests/runtests.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+clean() {
+    rm -rf /etc/firewalld/direct /etc/firewalld/icmptypes \
+           /etc/firewalld/ipsets /etc/firewalld/services \
+           /etc/firewalld/zones
+}
+
+fail() {
+    echo "Test '$1' failed with exit code '$2'"
+    exit $2
+}
+
+echo "*** This testsuite messes with your firewall settings so ***"
+echo "*** do not run it in production environments ***"
+
+sleep 5
+
+basedir="$(readlink -f ${0} | rev | cut -d '/' -f 4- | rev)"
+
+echo "Starting FirewallD server"
+# Kill existing one. We need to start the lastest one from the repo.
+while true; do
+    old_fwd=$(pgrep firewalld)
+    if [ "x" != "x$old_fwd" ]; then
+        kill $old_fwd > /dev/null 2>&1
+    else
+        break
+    fi
+done
+
+clean
+$basedir/src/firewalld || exit 1
+
+pushd $basedir/src/tests > /dev/null 2>&1
+for x in firewall*.sh firewall*.py; do
+    echo "Restarting FirewallD configuration"
+    $basedir/src/firewall-cmd -q --complete-reload
+    echo "Running test: $x"
+    # Pass 'y' to any asked questions
+    echo 'y' | ./$x || fail $x $?
+done
+popd > /dev/null 2>&1
+
+exit 0
